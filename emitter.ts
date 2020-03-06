22075e0f98c9456a1ab0b4e238e7989f4480f657
Radek Sedlák <r.sedlak@quadient.com>
2020-03-06 13:51:14 +0100

    Strongly typed EventEmitter

---
+++ emitter.ts
@@ -0,0 +1,46 @@
+class EventEmitter<EventsMap extends {} = any> {
+    on<Event extends keyof EventsMap>(event: Event, callback: EventsMap[Event]): void {
+        // do some stuff
+    }
+
+    // @ts-ignore
+    event<KEY extends keyof EventsMap>(event: KEY, args: Parameters<EventsMap[KEY]>): void {
+        // do some stuff
+    }
+}
+
+enum Events {
+    First = "firstEvent",
+    Second = "secondEvent"
+}
+
+/**
+ * Strongly typed
+ */
+const typedEventEmitter = new EventEmitter<{
+    [Events.First]: (a: number) => void,
+    [Events.Second]: () => void,
+}>();
+
+
+// with args
+typedEventEmitter.on(Events.First, (a: number) => {
+    void a;
+});
+typedEventEmitter.event(Events.First, [1]);
+
+// without args
+typedEventEmitter.on(Events.Second, () => {
+
+});
+typedEventEmitter.event(Events.Second, []);
+
+/**
+ * Default is not typed
+ */
+const notTypedEmitter = new EventEmitter();
+
+notTypedEmitter.on("evt", (a: number) => {
+
+});
+notTypedEmitter.event("evt", []);
