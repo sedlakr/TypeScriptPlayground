0b6292efce2f8e01f0c1a98c3bf3042eb2dc6082
Radek Sedlák <r.sedlak@quadient.com>
2020-03-09 11:39:36 +0100

    params checking + empty args enabled

--- emitter.ts
+++ emitter.ts
@@ -1,17 +1,17 @@
 class EventEmitter<EventsMap extends {} = any> {
-    on<Event extends keyof EventsMap>(event: Event, callback: EventsMap[Event]): void {
+    on<Event extends keyof EventsMap>(...[event, handler]: EventsMap[Event] extends (...args: infer P) => any ? (P extends [] ? [Event, () => void] : [Event, (P) => void]) : never): void {
         // do some stuff
     }
 
-    // @ts-ignore
-    event<Event extends keyof EventsMap>(event: Event, args: Parameters<EventsMap[Event]>): void {
+    event<Event extends keyof EventsMap>(...[event, args]: EventsMap[Event] extends (...args: infer P) => any ? (P extends [] ? [Event, void] : [Event, P]) : [Event, void]): void {
         // do some stuff
     }
 }
 
 enum Events {
     First = "firstEvent",
-    Second = "secondEvent"
+    Second = "secondEvent",
+    Third = "thirdEvent",
 }
 
 /**
@@ -20,20 +20,44 @@ enum Events {
 const typedEventEmitter = new EventEmitter<{
     [Events.First]: (a: number) => void,
     [Events.Second]: () => void,
+    [Events.Third]: (a: string) => void,
 }>();
 
 
+/**
+ * First
+ */
 // with args
 typedEventEmitter.on(Events.First, (a: number) => {
     void a;
 });
 typedEventEmitter.event(Events.First, [1]);
+// error
+typedEventEmitter.event(Events.First);
+typedEventEmitter.event(Events.First, ["b"]);
 
+/**
+ * Second
+ */
 // without args
 typedEventEmitter.on(Events.Second, () => {
 
 });
-typedEventEmitter.event(Events.Second, []);
+typedEventEmitter.event(Events.Second);
+
+// error
+typedEventEmitter.event(Events.Second, ["1"]);
+
+/**
+ * Third
+ */
+typedEventEmitter.event(Events.Third, ["b"]);
+
+// error
+typedEventEmitter.event(Events.Third);
+typedEventEmitter.event(Events.Third, []);
+typedEventEmitter.event(Events.Third, [1]);
+
 
 /**
  * Default is not typed
